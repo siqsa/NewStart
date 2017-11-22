b519ddcf4c072fd821fc73c6388d5f16d8d5e3e8
gotaehyuk <gotaehyuk@gmail.com>
2017-11-22 14:26:01 +0900

    Initial commit

---
+++ app/src/main/java/com/example/amdin/newstart/LoadingActivity.java
@@ -0,0 +1,23 @@
+package com.example.amdin.newstart;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        Handler handler= new Handler();
+        handler.postDelayed(new Runnable(){
+                                public void run(){
+                                    Intent intent = new Intent(getBaseContext(),LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();}}
+
+                ,3000);{}
+    }
+}
