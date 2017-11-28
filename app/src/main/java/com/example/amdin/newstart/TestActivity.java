dde68b16046b55bffcc5ea746a066622ef815444
gotaehyuk <gotaehyuk@gmail.com>
2017-11-28 14:32:09 +0900

    abcd

---
+++ app/src/main/java/com/example/amdin/newstart/TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.amdin.newstart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amdin on 2017-11-28.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
