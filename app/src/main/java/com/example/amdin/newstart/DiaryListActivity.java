357a8a01db2d3c7b6b1fbc716863e74cc0ec9de3
gotaehyuk <gotaehyuk@gmail.com>
2017-11-29 12:04:07 +0900

    add Diary

---
+++ app/src/main/java/com/example/amdin/newstart/DiaryListActivity.java
@@ -0,0 +1,18 @@
+package com.example.amdin.newstart;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by amdin on 2017-11-29.
+ */
+
+public class DiaryListActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+
+    }
+}
