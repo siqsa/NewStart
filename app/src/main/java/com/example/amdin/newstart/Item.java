b41eb30e1a197d65c0f515b477824a2ca828f83e
gotaehyuk <gotaehyuk@gmail.com>
2017-11-29 11:43:25 +0900

    add item

--- app/src/main/java/com/example/amdin/newstart/Item.java
+++ app/src/main/java/com/example/amdin/newstart/Item.java
@@ -5,4 +5,31 @@ package com.example.amdin.newstart;
  */
 
 public class Item {
+
+    String year;
+    String month;
+    String day;
+    String text;
+    public Item(String year,String month,String day,String text){
+        this.year=year;
+        this.month=month;
+        this.day=day;
+        this.text=text;
+    }
+    public void setYear(String year){this.year=year;}
+    public void setMonth(String month){this.month=month;}
+    public void setDay(String day){this.day=day;}
+    public void setText(String text){this.text=text;}
+    public String getYear(){
+        return year;
+    }
+    public String getMonth(){
+        return month;
+    }
+    public String getDay(){
+        return day;
+    }
+    public String getText(){
+        return text;
+    }
 }
