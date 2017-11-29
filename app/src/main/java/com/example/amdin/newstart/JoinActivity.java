5700c0b32a8183cbeee8b9a1553583b5116ff8ef
gotaehyuk <gotaehyuk@gmail.com>
2017-11-29 15:03:36 +0900

    Merge branch 'master' of C:\Program Files\NewStart with conflicts.

--- app/src/main/java/com/example/amdin/newstart/JoinActivity.java
+++ app/src/main/java/com/example/amdin/newstart/JoinActivity.java
@@ -4,20 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by tidy1 on 2017-11-28.
  */
 
 public class JoinActivity extends AppCompatActivity {
-    
 
+EditText email;
+EditText pwd;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
-
+    email=(EditText)findViewById(R.id.email);
+    pwd=(EditText)findViewById(R.id.password);
     }
     public void onJoin(View v){
 
