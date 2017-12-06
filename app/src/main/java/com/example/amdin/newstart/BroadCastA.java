4fd6cfabfbf3d4198e5291abdbaec17effa4c001
gotaehyuk <gotaehyuk@gmail.com>
2017-12-06 13:05:07 +0900

    change12

--- app/src/main/java/com/example/amdin/newstart/BroadCastA.java
+++ app/src/main/java/com/example/amdin/newstart/BroadCastA.java
@@ -1,8 +1,16 @@
 package com.example.amdin.newstart;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by amdin on 2017-12-06.
  */
 
-public class BroadCastA BroadcastReceiver {
+    public class BroadCastA extends BroadcastReceiver {
+    public void onReceive(Context context, Intent intent) {
+        Intent service_intent=new Intent(context,BroadcastS.class);
+        context.startService(service_intent);
+    }
 }
