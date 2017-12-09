231691eff407e6f34f1b33092525d3b85a4293e4
gotaehyuk <siqsa@naver.com>
2017-12-09 22:28:43 +0900

    toouch modify

--- app/src/main/java/com/example/amdin/newstart/DiaryListActivity.java
+++ app/src/main/java/com/example/amdin/newstart/DiaryListActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -52,6 +55,38 @@ public class DiaryListActivity extends AppCompatActivity {
         sp = getSharedPreferences("myFile", 00);
         number = sp.getLong("serial_number", 0);
         myRef = database.getReference("serial_number").child(number.toString());
+        final GestureDetector gestureDetector = new GestureDetector(DiaryListActivity.this,new GestureDetector.SimpleOnGestureListener()
+        {
+            @Override
+            public boolean onSingleTapUp(MotionEvent e)
+            {
+                return true;
+            }
+        });
+
+
+        출처: http://itpangpang.xyz/44 [ITPangPang]
+        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
+            @Override
+            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+                View child=rv.findChildViewUnder(e.getX(),e.getY());
+                if(child!=null&&gestureDetector.onTouchEvent(e)){
+                int touchPosition=rv.getChildLayoutPosition(child);
+                Toast.makeText(getApplicationContext(),touchPosition+"번째 레이아웃",Toast.LENGTH_SHORT).show();}
+                return false;
+            }
+
+            @Override
+            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            }
+
+            @Override
+            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+            }
+        });
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
